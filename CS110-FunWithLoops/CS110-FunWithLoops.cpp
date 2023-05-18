9ffc2f2a3bb27d3fdb7efc65ce1e96e9ac0ab90f
Taylor Bisset <taylorbisset@gmail.com>
2023-05-18 10:49:43 -0700

    Problem 1 complete. Print numbers 10 to 1.

--- CS110-FunWithLoops/CS110-FunWithLoops.cpp
+++ CS110-FunWithLoops/CS110-FunWithLoops.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
-    
+	// Problem 1: Print numbers 10 through 1 with spaces inbetween.
+	
+	for (int y = 10; y >= 1; y--)
+	{
+		cout << y << " ";
+	}
+
+	cout << endl;
 }
