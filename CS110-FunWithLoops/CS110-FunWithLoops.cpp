abcf429b5804bb3f8c2fc07089180ce4d3b11641
Taylor Bisset <taylorbisset@gmail.com>
2023-05-18 11:14:40 -0700

    Problem 2 complete. Echo user input.

--- CS110-FunWithLoops/CS110-FunWithLoops.cpp
+++ CS110-FunWithLoops/CS110-FunWithLoops.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	// Problem 1: Print numbers 10 through 1 with spaces inbetween.
+	// Problem 1: Print the numbers 10 to 1 with a space between each number.
 	
+	cout << "Problem 1: Print the numbers 10 to 1 with a space between each number.\n\n";
+
 	for (int y = 10; y >= 1; y--)
 	{
 		cout << y << " ";
 	}
 
-	cout << endl;
+	cout << endl << endl;
+
+	// Problem 2: Parrot: Echo the user's input until the user enters nothing.
+
+	cout << "Problem 2: Parrot: Echo the user's input until the user enters nothing.\n\n";
+
+	string parrot = "Parrot";
+
+	while (parrot != "")
+	{
+		cout << "You     : ";
+		getline(cin, parrot);
+		cout << "Parrot  : " << parrot << endl;
+	}
+	
+	cout << endl << endl;
 }
